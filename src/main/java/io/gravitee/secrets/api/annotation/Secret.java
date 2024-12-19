5971f929ec47d88936892148749fd38bbcdc15b2
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2024-12-19 17:31:15 +0100

    fix: add default secret kind

--- src/main/java/io/gravitee/secrets/api/annotation/Secret.java
+++ src/main/java/io/gravitee/secrets/api/annotation/Secret.java
@@ -16,7 +16,6 @@
 package io.gravitee.secrets.api.annotation;
 
 import io.gravitee.secrets.api.el.FieldKind;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
